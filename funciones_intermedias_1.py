fc8f8543ab61dac44e65e8a70e3b41a1b424602a
Ale <133436298+afuenteshinojosa@users.noreply.github.com>
2023-06-14 12:50:07 -0400

    funciones_intermedias_1

---
+++ funciones_intermedias_1.py
@@ -0,0 +1,54 @@
+x = [ [5,2,3], [15,8,9] ] 
+estudiantes = [
+    {'first_name':  'Michael', 'last_name' : 'Bryant'},
+    {'first_name' : 'John', 'last_name' : 'Rosales'}
+]
+directorio_deportes = {
+    'basketball' : ['Kobe', 'Jordan', 'James', 'Curry'],
+    'fútbol' : ['Andrés', 'Ronaldo', 'Rooney']
+}
+z = [ {'x': 10, 'y': 30} ]
+
+"""
+Iterar a través de una lista de diccionarios
+Crea una función iterateDictionary(some_list)para que, dada una lista de diccionarios, la función recorra cada diccionarios de la lista e imprima cada llave y el valor asociado.
+"""
+def iterateDictionary(some_list):
+    for dictionary in some_list:
+        for key, value in dictionary.items():
+            print(key, ":", value)
+
+estudiantes = [
+        {'first_name':  'Michael', 'last_name' : 'Jordan'},
+        {'first_name' : 'John', 'last_name' : 'Rosales'},
+        {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+        {'first_name' : 'KB', 'last_name' : 'Tonel'} 
+]
+
+iterateDictionary(estudiantes) 
+
+print("-------------")
+
+def iterateDictionary2(key_name, some_list):
+    for dictionary in some_list:
+        if key_name in dictionary:
+            print(dictionary[key_name])
+
+
+iterateDictionary2('first_name', estudiantes)
+iterateDictionary2('last_name', estudiantes)
+
+print("-------------")
+
+def printInfo(some_dict):
+    for key, value in some_dict.items():
+        print(len(value), key)
+        for item in value:
+            print(item)
+            
+directorio_deportes = {
+    'basketball' : ['Kobe', 'Jordan', 'James', 'Curry'],
+    'fútbol' : ['Andrés', 'Ronaldo', 'Rooney']
+}
+
+printInfo(directorio_deportes)
